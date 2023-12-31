055e351cdd7f692924c200708e50ee2585eeb9d0
ArsenKakasyan <archee.busy@gmail.com>
2023-12-31 18:54:59 +0300

    upd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from asyncio import run
 
 from ggchat import GigaChat_impl
+from openaichat import OpenAIChatImpl
 from kandinsky import Kadninsky_impl
 from dotenv import load_dotenv
 
@@ -12,21 +13,32 @@ load_dotenv()
 logging.basicConfig(level=logging.INFO)
 
 GIGACHAT_CLIENT_SECRET = os.getenv('GIGACHAT_CLIENT_SECRET')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 GIGACHAT_CREDENTIALS = os.getenv('GIGACHAT_CREDENTIALS')
 KANDINSKY_API_KEY = os.getenv('KANDINSKY_API_KEY')
 KADNINSKY_SECRET_KEY = os.getenv('KADNINSKY_SECRET_KEY')
 IMGUR_CLIENT_ID= os.getenv('IMGUR_CLIENT_ID')
+PROXY_LOGIN = os.getenv('PROXY_LOGIN')
+PROXY_PASSWORD = os.getenv('PROXY_PASSWORD')
+PROXY_IP = os.getenv('PROXY_IP')
+PROXY_PORT = os.getenv('PROXY_PORT')
 
 
 class Server:
     def __init__(self):
         self.app = Flask(__name__)
-        CORS(self.app, origins='http://0.0.0.0:3000')
+        CORS(self.app, origins='http://172.19.0.3:3000')
         self.gigachat = GigaChat_impl(GIGACHAT_CREDENTIALS, 'GIGACHAT_API_PERS', False)
+        self.openaichat = OpenAIChatImpl(OPENAI_API_KEY=OPENAI_API_KEY, PROXY_LOGIN=PROXY_LOGIN, PROXY_PASSWORD=PROXY_PASSWORD, PROXY_IP=PROXY_IP, PROXY_PORT=PROXY_PORT)
         self.kandinsky = Kadninsky_impl('https://api-key.fusionbrain.ai/', KANDINSKY_API_KEY, KADNINSKY_SECRET_KEY)
         self.setup_routes()
         
     def setup_routes(self):
+
+        @self.app.route('/call_openai', methods=['GET', 'OPTIONS'])
+        def call_openai():
+            result = run(self.openaichat.call_openai(request.args.get('action'), request.args.get('title'), request.args.get('units'), request.args.get('transcript')))
+            return jsonify(result)
         
         @self.app.route('/call_gigachat', methods=['GET', 'OPTIONS'])
         def call_gigachat():
@@ -44,4 +56,4 @@ class Server:
 
 if __name__ == '__main__':
     server = Server()
-    server.run(debug=True, port=8225, host='0.0.0.0')
+    server.run(debug=True, port=8225, host='172.19.0.5')
---
+++ openaichat.py
@@ -0,0 +1,153 @@
+
+
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import ResponseSchema, StructuredOutputParser
+import tenacity
+from tenacity import retry, stop_after_attempt, wait_fixed
+import httpx
+import json
+
+
+class OpenAIChatImpl:
+    def __init__(self, **kwargs):
+ 
+        self.openaichat = ChatOpenAI(api_key=kwargs.get('OPENAI_API_KEY'), 
+        http_client=httpx.Client(proxies=f'http://{kwargs.get("PROXY_LOGIN")}:{kwargs.get("PROXY_PASSWORD")}@{kwargs.get("PROXY_IP")}:{kwargs.get("PROXY_PORT")}/'), 
+        verbose=False, 
+        temperature=0.1, 
+        max_retries=3)
+
+    async def createUnitsNChapters(self, title, units):
+        units_list = units.split(",")
+        response_schemas = []
+
+        for i, unit in enumerate(units_list, start=1):
+            chapters_schema = ResponseSchema(name=f'chapters{i}', description=f'3 главы {i}-го раздела', type='List[{youtube_search_query: string, chapter_title: string}]')
+            response_schemas.append(chapters_schema)
+
+        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+        format_instructions = output_parser.get_format_instructions()
+        print(format_instructions)
+
+        template_string = """Ты - помощник, способный курировать содержание курса. \
+        Ты можешь придумывать соответствующие названия глав и придумывать поисковые запросы youtube для каждоый главы
+
+        Твоя задача - создать курс о ```{title}``` 
+        У тебя есть разделы курса ```{units}``` , тебе нужно сгенерировать для каждого раздела 3 новые главы на отдельные подтемы
+        Затем для каждой главы сгенерируй поисковый запрос в youtube_search_query и название chapter_title.
+
+        ФОРМАТ ОТВЕТА НИЖЕ, ПРИДЕРЖИВАЙСЯ ЕГО ИЛИ ТЕБЯ ВЫКЛЮЧАТ НАВСЕГДА!
+
+        {format_instructions}
+        """
+        prompt = ChatPromptTemplate.from_template(template=template_string)
+        messages = prompt.format_messages(title=title, 
+                                            units=units,
+                                            unitsLength=len(units),
+                                            format_instructions=format_instructions)
+        
+        response = self.openaichat(messages)
+        response_as_dict = output_parser.parse(response.content)
+        print(response_as_dict)
+        result = []
+
+        # Get the chapters from the response_as_dict
+        chapters_list = list(response_as_dict.values())
+
+        # Map the units to the chapters by order
+        result = [{"title": unit, "chapters": chapters} for unit, chapters in zip(units_list, chapters_list)]
+
+        # Convert the result to JSON
+        result_json = json.dumps(result, indent=4)
+
+        return result_json
+
+        # What we get here
+        """[
+            { title: 'functions', chapters: [ [Object], [Object], [Object] ] },
+            { title: 'classes', chapters: [ [Object], [Object], [Object] ] },
+            { title: 'decorators', chapters: [ [Object], [Object], [Object] ] }
+        ]"""
+
+    
+    async def createImageSearchTerm(self, title):
+        response_schemas = []
+        chapters_schema = ResponseSchema(name=f'image_search_term', description=f'a good prompt for course thumbnail generation', type='{image_search_term: string}')
+        response_schemas.append(chapters_schema)
+
+        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+        format_instructions = output_parser.get_format_instructions()
+        print(format_instructions)
+
+        template_string = """You are an assistant capable of evaluating the best prompt for course thumbnail generation. \
+        Please provide a good prompt for midjourney to generate a good image about: ```{title}``` 
+
+        Return a JSON object with only 1 key and 1 value, nothing else.
+
+        {format_instructions}
+        """
+        prompt = ChatPromptTemplate.from_template(template=template_string)
+        messages = prompt.format_messages(title=title, 
+                                            format_instructions=format_instructions)
+        
+        response = self.openaichat(messages)
+        response_as_dict = output_parser.parse(response.content)
+        print(response.content)
+        print(response_as_dict)
+
+        # Convert the result to JSON
+        result_json = json.dumps(response_as_dict, indent=4)
+
+        return result_json
+
+        # What we get here
+        """[ { image_search_term: 'a good search term for the title of the course' } ]"""
+        
+    async def createYoutubeSummary(self, transcript):
+        response_schemas = []
+        chapters_schema = ResponseSchema(name=f'summary', description=f'краткое содержание транскрипта', type='your_summary')
+        response_schemas.append(chapters_schema)
+
+        output_parser = StructuredOutputParser.from_response_schemas(response_schemas)
+        format_instructions = output_parser.get_format_instructions()
+        print(format_instructions)
+
+        template_string = """Ты - помощник, способный делать краткое содержание транскрипта youtube видео.  \
+        Сделай краткое содержание в 250 словах или менее, не говори о спонсорах или рекламе, не имеющих отношения к основной теме текста далее 
+
+        ```{transcript}```
+               
+
+        В ответе верни JSON объект с одним ключем и одним значением.
+        
+        {format_instructions}
+        """
+        prompt = ChatPromptTemplate.from_template(template=template_string)
+        messages = prompt.format_messages(transcript=transcript, 
+                                            format_instructions=format_instructions)
+        
+        response = self.openaichat(messages)
+        print(response.content)
+        response_as_dict = output_parser.parse(response.content)
+
+        
+        print(response_as_dict)
+
+        # Convert the result to JSON
+        result_json = json.dumps(response_as_dict, indent=4)
+
+        return result_json
+
+        # What we get here
+        """[ { summary: 'краткое содержание видео' } ]"""
+
+    async def call_openai(self, action, title, units, transcript):
+        if action == 'createUnitsNChapters':
+            return await self.createUnitsNChapters(title, units)
+        elif action == 'createImageSearchTerm':
+            return await self.createImageSearchTerm(title)
+        elif action == 'createYoutubeSummary':
+            return await self.createYoutubeSummary(transcript)
+        
